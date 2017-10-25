1943c2228246ee766906f67764799f1758990551
Devanand T <devanand.t.1986@gmail.com>
2017-10-25 21:49:05 +0530

    Update slurm_nandadevi.sh

--- SLURM_JOB_SUBMISSION_NAMD/slurm_nandadevi.sh
+++ SLURM_JOB_SUBMISSION_NAMD/slurm_nandadevi.sh
@@ -1,1 +1,22 @@
 
+#!/bin/bash
+
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=28
+#SBATCH --job-name=pH4.5_E39A_300K
+#SBATCH --partition=nandaq
+
+#SBATCH --export=all
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=devanand.t.1986@gmail.com
+
+
+cd $SLURM_SUBMIT_DIR
+cat $SLURM_JOB_NODELIST > hostfile_$SLURM_JOBID
+
+#MPI Case
+module unload intel/2017
+module load intel/2015
+module load namd/2.10
+
+mpirun -v -np 56 /opt/apps/namd/2.10/namd2 ./2KO2_pH4.5_E39A_300K.config  >& 2KO2_pH4.5_E39A_300K.out
