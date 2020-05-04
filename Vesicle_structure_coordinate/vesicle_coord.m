1f5f54297badb3f0654198bac95e5dbf63b4795b
Devanand T <devanand.t.1986@gmail.com>
2020-05-04 09:43:00 +0530

    Update vesicle_coord.m

--- Vesicle_structure_coordinate/vesicle_coord.m
+++ Vesicle_structure_coordinate/vesicle_coord.m
@@ -10,9 +10,9 @@
 
 clear,clc
 
-Ap=30^2;
-r = 10; % radius of the sphere
-n=round((500/Ap)*4*pi*r^2);% number of points
+apl=1.9;
+r = 15; % radius of the sphere
+n=round((4*pi*r^2)/apl);% number of points
 u1=rand(1,n);
 u2=rand(1,n);
 lambda=acos(2.*u1-1)-(pi/2);
@@ -24,13 +24,13 @@ yp1    = r.*cos(lambda).*sin(phi);
 zp1    = r.*sin(lambda);  
 
 
-r = 9; % radius of the sphere
+r = r-1; % radius of the sphere
 xp2    = r.*cos(lambda).*cos(phi);
 yp2    = r.*cos(lambda).*sin(phi);
 zp2    = r.*sin(lambda);  
 
 
-r = 8; % radius of the sphere
+r = r-1; % radius of the sphere
 xp3    = r.*cos(lambda).*cos(phi);
 yp3    = r.*cos(lambda).*sin(phi);
 zp3    = r.*sin(lambda); 
@@ -42,29 +42,29 @@ y1=reshape(y1,[1 numel(y1)]);
 z1=[zp1;zp2;zp3];
 z1=reshape(z1,[1 numel(z1)]);
 
-r = 5.8; % radius of the sphere
-n=round((500/Ap)*4*pi*r^2);% number of points
+apl=1.8;
+r = 10; % radius of the sphere
+n=round((4*pi*r^2)/apl);% number of points
 u1=rand(1,n);
 u2=rand(1,n);
 lambda=acos(2.*u1-1)-(pi/2);
 phi=2*pi.*u2;
 
-r = 7.8; % radius of the sphere
-xp4    = r.*cos(lambda).*cos(phi);
-yp4    = r.*cos(lambda).*sin(phi);
-zp4    = r.*sin(lambda);  
 
+xp6    = r.*cos(lambda).*cos(phi);
+yp6    = r.*cos(lambda).*sin(phi);
+zp6    = r.*sin(lambda); 
 
-r = 6.8; % radius of the sphere
+r = r+1; % radius of the sphere
 xp5    = r.*cos(lambda).*cos(phi);
 yp5    = r.*cos(lambda).*sin(phi);
 zp5    = r.*sin(lambda);
 
+r=r+1;
+xp4    = r.*cos(lambda).*cos(phi);
+yp4    = r.*cos(lambda).*sin(phi);
+zp4    = r.*sin(lambda);  
 
-r = 5.8; % radius of the sphere
-xp6    = r.*cos(lambda).*cos(phi);
-yp6    = r.*cos(lambda).*sin(phi);
-zp6    = r.*sin(lambda); 
 
 x2=[xp4;xp5;xp6];
 x2=reshape(x2,[1 numel(x2)]);
@@ -81,7 +81,7 @@ XYZ=[x;y;z]';
 N=length(XYZ);
 
 
-fID = fopen('vesicle_rad10_apl1.8.xyz','w');
+fID = fopen('vesicle_rad15.xyz','w');
 fprintf(fID,'%d\n',N);  %to add number of atoms to first line
 fprintf(fID,'\n');      %to leave one blank line
 for i=1:N
